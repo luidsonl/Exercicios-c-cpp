6a7a5ace18d47fd34862110369e972634437feb3
Luidson Lima Santos <luidsonlsti@gmail.com>
2023-04-13 18:05:31 -0300

    Reorganizando arquivos

---
+++ Bootcamp-Programador-de-Computador/Modulo 4/trabalho_pratico/main.c
@@ -0,0 +1,12 @@
+/*Desenvolvimento de uma lista simplesmente encadeada, implementando
+um algoritmo com TAD dinâmico através de ponteiros.
+Requisitos:
+1. O programa deve permitir que o usuário insira valores inteiros na lista.
+2. O programa deve permitir que o usuário remova valores inteiros da lista.
+3. O programa deve exibir os valores da lista na tela em ordem crescente.
+4. O programa deve exibir uma mensagem de erro caso a lista esteja vazia
+ou o valor a ser removido não esteja presente na lista.
+5. O programa deve liberar a memória alocada para a lista quando
+finalizado.
+*/
+
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Exercícios comentados feitos com C++
+# Exercícios feitos com C e C++
 
 * Bootcamp Programador de Computador
 	* Bootcamp da graduação de Ciência de Dados da XPE.
